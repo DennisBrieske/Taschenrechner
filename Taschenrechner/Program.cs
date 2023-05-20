b2a0f2b58e192eb210d2bd15c424ca909b6da7d7
Dennis Brieske <Dennis.brieske@gmx.de>
2023-05-20 16:33:13 +0200

    Methode HoleSummanden hinzugefügt

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -8,25 +8,41 @@ namespace Taschenrechner
 {
     internal class Program
     {
+        // METHODEN DEFINIEREN (in 7 Schritten)
+        // (optional) Modifizierer definieren
+        // Datentyp des Rückgabewertes definieren
+        // Methodennamen definieren
+        // Rundeklammern an den Methodennamen anfügen
+        // Überlegen welche Parameter benötigt werden (optional diese definieren)
+        // Geschweifte Klammern einfügen
+        // Methode implementieren (Anweisungen in den Methodenrumpf schreiben)
+
         static void Main(string[] args)
         {
             // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen
-            Console.Write("Bitte gib die erste Zahl ein: ");
-            string zahl1 = Console.ReadLine();
-            Console.Write("Bitte gib die zweite Zahl ein: ");
-            string zahl2 = Console.ReadLine();
+
+            string ersterSummand = HoleSummanden("Bitte gib die erste Zahl ein: ");
+            string zweiterSummand = HoleSummanden("Bitte gib die zweite Zahl ein: ");
 
             // Wandel Text in Gleitkommazahl
-            double ersteGanzZahl = Convert.ToDouble(zahl1);
-            double zweiteGanzZahl = Convert.ToDouble(zahl2);
+            double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
+            double zweiterSummandAlsZahl = Convert.ToDouble(zweiterSummand);
 
             // Berechnung ausführen
-            double summe = Addiere(ersteGanzZahl, zweiteGanzZahl);
+            double summe = Addiere(ersterSummandAlsZahl, zweiterSummandAlsZahl);
 
             // Ausgabe der Summe
             Console.WriteLine("Die Summe ist: {0}", summe);
 
             WarteAufBenutzerEingabe();
+        }
+
+        static string HoleSummanden(string ausgabeText)
+        {
+            Console.Write(ausgabeText);
+            string summand = Console.ReadLine();
+
+            return summand;
         }
 
         static double Addiere(double ersterSummand, double zweiterSummand)
